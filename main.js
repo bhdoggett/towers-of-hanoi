e1aec15dc7d83ce5ded713482ac139bb3bf65124
Ben Doggett <bdoggett@gmail.com>
2024-11-03 14:43:39 -0500

    Updateing main.js to fix some code problems

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-// console.log("let's do this!");
-
 const Towers = function (name, pegs, discs) {
   this.name = name;
   this.discs = discs;
   this.pegs = pegs;
   this.moveCount = 0;
   this.initializeBoard = () => {
-    let towers = [];
+    const towers = [];
 
     for (let i = 0; i < this.pegs; i++) {
       towers.push([]);
@@ -20,16 +18,15 @@ const Towers = function (name, pegs, discs) {
   };
   this.towers = this.initializeBoard();
   this.printBoard = function () {
-    this.towers.map((tower) => console.log("---", ...tower));
+    this.towers.forEach((tower) => console.log("---", ...tower));
   };
-  this.shouldAutoPlay = true; // we need a condition for the autoPlay method to break the loop if the game has been won. This property's value will change to false if the game is won.
+  this.shouldAutoPlay = true;
 };
 
 Towers.prototype.moveDisc = function (from, to) {
   let possible = true;
   let returnStatement = "";
 
-  // let towers = this.towers;
   if (!this.towers[from - 1][this.towers[from - 1].length - 1]) {
     possible = false;
     returnStatement =
@@ -55,30 +52,22 @@ Towers.prototype.moveDisc = function (from, to) {
 
   if (to !== 1 && this.towers[to - 1].length === this.discs) {
     returnStatement = `YOU WIN! The ${this.name} Towers board has been reset to it's original position. Play again?`;
-    // for (let j = this.discs; j > 0; j--) {
-    //   this.towers[0].push(j);
-    // }
-    // this.towers[to - 1].splice(0, this.discs);
+
     this.towers = this.initializeBoard();
-    this.printBoard();
-    this.shouldAutoPlay = false; // this will update the object's property to be used as a condition in the autoPlay loop
+    this.shouldAutoPlay = false;
   }
 
+  this.moveCount++;
   console.log(returnStatement);
   this.printBoard();
-  return true;
 };
 
 Towers.prototype.autoPlay = function (numMoves) {
-  let moveCount = 0;
-
-  let randomMove = () => {
-    let moveFrom = Math.ceil(Math.random() * this.towers.length); //
-    let moveTo = Math.ceil(Math.random() * this.towers.length); // finish randomizing the move choices to be flexible with how many towers there are
+  const randomMove = () => {
+    const moveFrom = Math.ceil(Math.random() * this.towers.length);
+    const moveTo = Math.ceil(Math.random() * this.towers.length);
 
     play = this.moveDisc(moveFrom, moveTo);
-
-    return moveCount;
   };
 
   for (let i = 0; i < numMoves; i++) {
